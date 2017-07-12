a01159de1bc5f6bb8822acd8d750f9d3260781fc
Aniruddha Deshpande <adesh@uw.edu>
2017-07-12 09:53:35 -0700

    fixed qsub call

--- 02_resample/parent.R
+++ 02_resample/parent.R
@@ -1,17 +1,16 @@
 nodes <- 'geos'
 proj <- ifelse(nodes == 'geos',
-        '-P proj_geo_nodes -l gn = TRUE',                      		
+        '-P proj_geo_nodes -l gn=TRUE',                      		
         '-P proj_geospatial')
-proj <- "-P proj_geospatial"
 user <- "adesh"
 
 setwd('/share/code/geospatial/adesh/wash_mapping/02_resample/')
 indicators <- c("water")
 run_date <- Sys.Date()
+polydat <- read.csv('/home/j/WORK/11_geospatial/wash/data/agg/water_poly_agg_2017-07-11.csv')
+
 
 for (indic in indicators) {
-  polydat <- read.csv('/home/j/WORK/11_geospatial/wash/data/agg/water_poly_agg_2017-07-11.csv')
-  
   for (shp in unique(polydat$shapefile)) { 
     jname <- paste(indic, shp, sep = "_")
     mycores <- 4
