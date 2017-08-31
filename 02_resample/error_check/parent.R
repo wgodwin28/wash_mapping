b216b0e95398bdb98b366258841ef4b11f349879
Aniruddha Deshpande <adesh@uw.edu>
2017-08-31 16:46:00 -0700

    added location code error check

---
+++ 02_resample/error_check/parent.R
@@ -0,0 +1,38 @@
+# Shapefile and Location Code Checker: Parent
+rm(list = ls())
+
+# Input filepath for collapsed polydata
+load('J:/LIMITED_USE/LU_GEOSPATIAL/collapsed/wash/polydat_2017_08_30.RData')
+
+# Input survey shapefile library
+shp_dir <- 'J:/WORK/11_geospatial/05_survey shapefile library/Shapefile directory/'
+
+
+# 1. Check if all shapefile names are valid
+setwd(shp_dir)
+all_shp <- gsub('.shp','',list.files())
+wrong_shp <- setdiff(polydat$shapefile, all_shp)
+
+mydat <- filter(polydat, !(shapefile %in% wrong_shp))
+
+nodes <- 'geos'
+proj <- ifelse(nodes == 'geos',
+               '-P proj_geo_nodes -l gn=TRUE',                      		
+               '-P proj_geospatial')
+user <- "adesh"
+setwd('/share/code/geospatial/adesh/wash_mapping/02_resample/error_check')
+run_date <- Sys.Date()
+
+for (shp in unique(mydat$shapefile)) { 
+  jname <- paste(shp, sep = "_")
+  mycores <- 4
+  sys.sub <- paste0("qsub ",proj,paste0(" -e /share/temp/sgeoutput/",user,"/errors -o /share/temp/sgeoutput/",user,"/output "),
+                    "-cwd -N ", jname, " ", "-pe multi_slot ", mycores, " ")
+  script <- "child.R"
+  r_shell <- ifelse(nodes == 'geos',
+                    'r_shell_geos.sh',                      		
+                    'r_shell.sh')
+  
+  args <- paste(shp, run_date)
+  # system(paste(sys.sub, r_shell, script, args)) 
+}
