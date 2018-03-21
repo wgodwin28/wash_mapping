79159d408b6bea8a2e8124ddb9749dc5f5b0e1e9
Aniruddha Deshpande <deshpande.ani111@gmail.com>
2018-03-20 23:32:03 -0700

    put sani before water for both option in combine cr models

--- 05_utilities/combine_cr_ordinal.R
+++ 05_utilities/combine_cr_ordinal.R
@@ -85,8 +85,43 @@ if (indic == 'sani') {
 }
 
 if (indic == 'both') {
+    
     run_date <- commandArgs()[5]
- run_date <- as.character(commandArgs()[5])
+    for (region in c('cssa','essa_hilo','sssa_hi','wssa','name_hi')) {
+        for (indicator in c('s_imp', 's_unimp_cr')) {
+            message(region); message(indicator); 
+            
+            message('loading data...')
+            setwd(paste0('/share/geospatial/mbg/wash/',indicator,'/output/',run_date))
+            files <- list.files()[grep(pattern = 'cell_draws', x = list.files())]
+            preds <- load(files[grep(x = files, pattern = region)])
+            del_obj <- preds
+            preds <- get(preds)
+            assign(indicator, preds); rm(preds)
+            rm(list = paste(del_obj)); rm(del_obj);
+        }
+
+        message('calculating...')
+        s_unimp_calc <- (1 - s_imp) * s_unimp_cr; rm(s_unimp_cr)
+        s_od_calc <- 1 - s_imp - s_unimp_calc
+
+        message('saving s_unimp_calc...')
+        dir.create(paste0('/share/geospatial/mbg/wash/s_unimp_calc/output/',run_date))
+        setwd(paste0('/share/geospatial/mbg/wash/s_unimp_calc/output/',run_date))
+        save(s_unimp_calc, file = paste0("s_unimp_calc_cell_draws_eb_bin0_",
+                                    region,"_0.RData"))
+        rm(s_unimp_calc)
+
+        message('saving s_od_calc...')
+        dir.create(paste0('/share/geospatial/mbg/wash/s_od_calc/output/',run_date))
+        setwd(paste0('/share/geospatial/mbg/wash/s_od_calc/output/',run_date))
+        save(s_od_calc, file = paste0("s_od_calc_cell_draws_eb_bin0_",
+                                    region,"_0.RData"))
+        rm(s_od_calc); rm(s_imp)
+        
+    }
+
+    run_date <- as.character(commandArgs()[5])
     for (region in c('cssa','essa_hilo','sssa_hi','wssa','name_hi')) {
         for (indicator in c('w_piped_cr', 'w_imp','w_unimp_cr')) {
             message(region); message(indicator); 
@@ -126,41 +161,6 @@ if (indic == 'both') {
         save(w_surface_calc, file = paste0("w_surface_calc_cell_draws_eb_bin0_",
                                     region,"_0.RData"))
         rm(w_surface_calc); rm(w_piped)
-    }
-
-    run_date <- as.character(commandArgs()[5])
-    for (region in c('cssa','essa_hilo','sssa_hi','wssa','name_hi')) {
-        for (indicator in c('s_imp', 's_unimp_cr')) {
-            message(region); message(indicator); 
-            
-            message('loading data...')
-            setwd(paste0('/share/geospatial/mbg/wash/',indicator,'/output/',run_date))
-            files <- list.files()[grep(pattern = 'cell_draws', x = list.files())]
-            preds <- load(files[grep(x = files, pattern = region)])
-            del_obj <- preds
-            preds <- get(preds)
-            assign(indicator, preds); rm(preds)
-            rm(list = paste(del_obj)); rm(del_obj);
-        }
-
-        message('calculating...')
-        s_unimp_calc <- (1 - s_imp) * s_unimp_cr; rm(s_unimp_cr)
-        s_od_calc <- 1 - s_imp - s_unimp_calc
-
-        message('saving s_unimp_calc...')
-        dir.create(paste0('/share/geospatial/mbg/wash/s_unimp_calc/output/',run_date))
-        setwd(paste0('/share/geospatial/mbg/wash/s_unimp_calc/output/',run_date))
-        save(s_unimp_calc, file = paste0("s_unimp_calc_cell_draws_eb_bin0_",
-                                    region,"_0.RData"))
-        rm(s_unimp_calc)
-
-        message('saving s_od_calc...')
-        dir.create(paste0('/share/geospatial/mbg/wash/s_od_calc/output/',run_date))
-        setwd(paste0('/share/geospatial/mbg/wash/s_od_calc/output/',run_date))
-        save(s_od_calc, file = paste0("s_od_calc_cell_draws_eb_bin0_",
-                                    region,"_0.RData"))
-        rm(s_od_calc); rm(s_imp)
-        
     }
 
 }
